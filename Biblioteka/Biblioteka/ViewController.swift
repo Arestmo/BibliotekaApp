5a46505537b386b30b1dfda24d412be402bb2a64
Antiquiti <pawel.maniowiec@gmail.com>
2020-06-02 19:37:05 +0200

    project creation

---
+++ Biblioteka/Biblioteka/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Biblioteka
+//
+//  Created by Pawel on 02.06.2020.
+//  Copyright © 2020 PSW. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
