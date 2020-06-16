2ece333a6116556160cca8246f7f620645a0aa2a
Antiquiti <pawel.maniowiec@gmail.com>
2020-06-16 21:56:50 +0200

    Created LaunchScreen, Main Page and Filters

---
+++ Biblioteka/Biblioteka/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  Biblioteka
+//
+//  Created by Pawel on 07.06.2020.
+//  Copyright © 2020 PSW. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var bookImage: UIImageView!
+    @IBOutlet weak var bookName: UILabel!
+}
--- Biblioteka/Biblioteka/ViewController.swift
+++ Biblioteka/Biblioteka/ViewController.swift
@@ -8,13 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var search: UITextField!
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let books = ["book1", "book2", "book3", "book4"]
+    let bookImages: [UIImage] = [
+        UIImage(named: "book1")!,
+        UIImage(named: "book1")!,
+        UIImage(named: "book1")!,
+        UIImage(named: "book1")!,
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 35, height: 30))
+        imageView.image = UIImage(named: "lupa")
+        search.rightView = imageView;
+        search.rightViewMode = .always
     }
-
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return books.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
+        cell.bookName.text = books[indexPath.item]
+        cell.bookImage.image = bookImages[indexPath.item]
+        
+        return cell
+    }
 
 }
 
